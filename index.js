2d50578f41b1e1bb87a2455ccba1a525a7e741ee
Danish Mir <76698292+mirdanish6594@users.noreply.github.com>
2023-05-20 08:42:09 +0530

    minor changes in cards
    Changed cards from to table to flexbox

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+/*$(document).ready(function() {
+	// Will wait for everything on the page to load.
+	$(window).bind('load', function() {
+		$('.overlay-anim, body').addClass('loaded');
+		setTimeout(function() {
+			$('.overlay-anim').addClass('d-none');
+		}, 2000)
+	});
+	// Will remove overlay after 1min for users cannnot load properly.
+	setTimeout(function() {
+		$('.overlay-anim, body').addClass('loaded');
+	}, 3000);
+});*/
